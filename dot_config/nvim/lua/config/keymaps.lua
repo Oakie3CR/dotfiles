38f4ce975e3b3cc19ba03b5d469af0eed954b4cd
Christian Rigdon <christian.rigdon@octanner.com>
2024-06-18 14:08:58 -0600

    I like using default keys for <S-l> and <S-h>

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,11 @@ local map = vim.keymap.set
 -- convenience mappings
 map("n", ";", ":")
 
+-- I like having <S-h> go to the top of the screen and <S-l> go to the bottom of the screen
+-- I'll get used to the `[b` and `]b` commands
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
 -- Start lazy color scheme picker
 local telescope = require("telescope.builtin")
 local function openColorschemePickerWithEvent()
