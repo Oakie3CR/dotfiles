9f744b9db55f8cdf4fa5e25b9f2ed3942a10f47f
Christian Rigdon <christian.rigdon@octanner.com>
2024-12-02 09:50:33 -0700

    Get rid of F1 and make neovide copy past work

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,6 +7,7 @@ map("n", ";", ":")
 -- I'll get used to the `[b` and `]b` commands
 vim.keymap.del("n", "<S-h>")
 vim.keymap.del("n", "<S-l>")
+map("n", "<F1>", "<Esc>")
 
 -- Start lazy color scheme picker
 local telescope = require("telescope.builtin")
@@ -19,3 +20,18 @@ end
 -- Set the keymap to call the function
 map("n", "<leader>uC", openColorschemePickerWithEvent, { desc = "Colorscheme with preview" })
 -- End lazy color scheme picker
+
+-- neovide doesn't have these set by default like the terminal does
+if vim.g.neovide then
+  vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+end
