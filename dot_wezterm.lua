06454e4e55840f3ed6019cc7556cae1d093a1143
Christian Rigdon <christian.rigdon@octanner.com>
2025-12-05 13:31:40 -0700

    Adding tmux like controls to wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,9 +1,13 @@
 ---@type Wezterm
-local wezterm = require("wezterm")
-local act = wezterm.action
+local w = require("wezterm")
+
+---@type Action
+local act = w.action
 
 ---@type Config
-local config = wezterm.config_builder()
+local config = w.config_builder()
+
+local smart_splits = w.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 
 config.audible_bell = "Disabled"
 config.color_scheme = "Monokai Pro (Gogh)"
@@ -11,14 +15,83 @@ config.font_size = 13
 config.initial_cols = 160
 config.initial_rows = 60
 
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
 config.keys = {
+  -- clear the scrollback history
   {
     key = "k",
     mods = "CMD",
     action = act.ClearScrollback("ScrollbackAndViewport"),
   },
+  {
+    -- split pane vertically
+    -- tmux uses '%' for vertical split
+    -- other popular options are 'v' and '|'
+    key = "\\",
+    mods = "LEADER",
+    -- if I want to change to the pipe, then the mods like would look like
+    -- mods = "LEADER|SHIFT",
+    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    -- split pane horizontally
+    -- tmux uses '"' for horizontal split
+    -- other popular options are 'h' and '='
+    key = "-",
+    mods = "LEADER",
+    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    -- maximize one window
+    key = "m",
+    mods = "LEADER",
+    action = act.TogglePaneZoomState,
+  },
+  {
+    -- rotate panes clockwise
+    key = "Space",
+    mods = "LEADER",
+    action = act.RotatePanes("Clockwise"),
+  },
+  {
+    -- show the pane selection mode, and then have it swap active and selected panes
+    mods = "LEADER",
+    key = "0",
+    action = act.PaneSelect({
+      mode = "SwapWithActive",
+    }),
+  },
+  {
+    -- vim mode in the terminal so I can copy from the history
+    key = "Enter",
+    mods = "LEADER",
+    action = act.ActivateCopyMode,
+  },
 }
 
 config.term = "wezterm"
+
+smart_splits.apply_to_config(config, {
+  -- the default config is here, if you'd like to use the default keys,
+  -- you can omit this configuration table parameter and just use
+  -- smart_splits.apply_to_config(config)
+
+  -- directional keys to use in order of: left, down, up, right
+  direction_keys = { "h", "j", "k", "l" },
+  -- if you want to use separate direction keys for move vs. resize, you
+  -- can also do this:
+  -- direction_keys = {
+  --   move = { 'h', 'j', 'k', 'l' },
+  --   resize = { 'LeftArrow', 'DownArrow', 'UpArrow', 'RightArrow' },
+  -- },
+  -- modifier keys to combine with direction_keys
+  modifiers = {
+    move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+    resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+  },
+  -- log level to use: info, warn, error
+  log_level = "info",
+})
 
 return config
